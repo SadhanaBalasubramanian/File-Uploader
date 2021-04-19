1024a25db8197a55527f80c9301788503a377bdc
SadhanaBalasubramanian <sadhanabalu86@gmail.com>
2021-04-19 10:12:10 +0530

    Added default files package.jon,package.json and package-lock.json

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import fileUpload from "express-fileupload";
+
+const app=express();
+
+app.use(fileUpload());
+
+
+//Upload Endpoint
+app.post("/upload",(req,res)=>{
+    if(req.files==null){
+        return res.status(400).json({msg:"No file uploaded"})
+    }
+
+    const file=req.files.file;
+
+    file.mv(`${__dirname}/client/public/uploads/${filename}`,err =>{
+        if(err){
+            console.error(err);
+            return res.status(500).send(err);
+        }
+        res.json({fileName: file.name,filePath:`/upload/${file.name}`});
+    })
+})
+
+app.listen(5000,()=> console.log("Server Started.."));
